4acf67547b63eb824faa87449f7a1a7ac80fc045
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-08-26 19:12:42 +0200

    spinners for all

--- assets/back.js
+++ assets/back.js
@@ -2,12 +2,9 @@ const productId = new URLSearchParams(window.location.search).get("ProdId");
 const url = productId
   ? `https://striveschool-api.herokuapp.com/api/product/${productId}`
   : "https://striveschool-api.herokuapp.com/api/product";
-let campiInput = document.getElementsByTagName("input");
-let form = document.querySelector("form");
 let card = document.querySelector(".card");
+
 window.onload = async () => {
-  let eliminate = document.getElementById("delete");
-  let modifiOcrea = document.getElementById("creaOModifica");
   fetch(url, {
     headers: {
       Authorization:
@@ -18,82 +15,113 @@ window.onload = async () => {
     if (strprod.status === 401) {
       console.log("la richiesta non è stata trovata");
       card.innerHTML = `<div class="card text-center">
-      <div class="card-header">
+        <div class="card-header">
         STOP!
-      </div>
-      <div class="card-body">
+        </div>
+        <div class="card-body">
         <h5 class="card-title">ERROR 401 </h5>
         <p class="card-text">ehi hai eseguito una richiesta non valida preparati a finire in gattabuia</p>
         <a href="index.html" class="btn btn-primary">no! non farlo! torno indietro</a>
-      </div>
-      <div class="card-footer text-body-secondary">
+        </div>
+        <div class="card-footer text-body-secondary">
         401
-      </div>
-    </div>`;
+        </div>
+        </div>`;
     }
     if (strprod.status === 404) {
       console.log("la richiesta non è stata trovata");
       card.innerHTML = `<div class="card text-center">
-      <div class="card-header">
+        <div class="card-header">
         fatal error
-      </div>
-      <div class="card-body">
+        </div>
+        <div class="card-body">
         <h5 class="card-title">ERROR 404 </h5>
         <p class="card-text">la pagina che cercavi non è stata trovata a meno che non cercavi proprio questa pagina in quel caso bravo l'hai trovata</p>
         <a href="index.html" class="btn btn-primary">torna indietro</a>
-      </div>
-      <div class="card-footer text-body-secondary">
+        </div>
+        <div class="card-footer text-body-secondary">
         404
-      </div>
-    </div>`;
+        </div>
+        </div>`;
     }
     if (strprod.status === 500) {
       console.log("generic server error");
       card.innerHTML = `<div class="card text-center">
-      <div class="card-header">
+        <div class="card-header">
         fatal error
-      </div>
-      <div class="card-body">
+        </div>
+        <div class="card-body">
         <h5 class="card-title">ERROR 500 </h5>
         <p class="card-text">un'errore generico del server hmmm molto strano</p>
         <a href="index.html" class="btn btn-primary">torna indietro</a>
-      </div>
-      <div class="card-footer text-body-secondary">
+        </div>
+        <div class="card-footer text-body-secondary">
         500
-      </div>
-    </div>`;
+        </div>
+        </div>`;
     }
     if (strprod.status === 503) {
       console.log("server gets stop");
       card.innerHTML = `<div class="card text-center">
-      <div class="card-header">
+        <div class="card-header">
         fatal error
-      </div>
-      <div class="card-body">
+        </div>
+        <div class="card-body">
         <h5 class="card-title">ERROR 503 </h5>
         <p class="card-text">il criceto che alimentava il server è andato in pausa caffè stiamo lavorando per farlo ripartire</p>
         <a href="index.html" class="btn btn-primary">torna indietro</a>
-      </div>
-      <div class="card-footer text-body-secondary">
+        </div>
+        <div class="card-footer text-body-secondary">
         503
-      </div>
-    </div>`;
+        </div>
+        </div>`;
     }
     if (strprod.status === 418) {
       console.log("server gets stop");
       card.innerHTML = `<div class="card text-center">
-      <div class="card-header">
+        <div class="card-header">
         fatal error
-      </div>
-      <div class="card-body">
+        </div>
+        <div class="card-body">
         <h5 class="card-title">ERROR 418</h5>
         <p class="card-text">il server si rifiuta di farti un caffè perchè è un server inospitale (oltre che è una teiera da the)</p>
         <a href="index.html" class="btn btn-primary">fammi un thè invece</a>
-      </div>
-      <div class="card-footer text-body-secondary">
+        </div>
+        <div class="card-footer text-body-secondary">
         418
+        </div>
+        </div>`;
+    }
+
+    if (strprod.ok) {
+      card.innerHTML = `<form>
+      <div class="container">
+        <label for="nome">titolo</label>
+        <input type="text" placeholder="nome" class="form-control" id="nome" required />
+
+        <label for="genere">genere</label>
+        <input type="text" placeholder="genere" class="form-control" id="genere" required />
+
+        <label for="brand">brand</label>
+        <input type="text" placeholder="brand" class="form-control" id="brand" required />
+
+        <label for="indirizzo immagine">indirizzo immagine</label>
+        <input type="text" placeholder="indirizzo immagine" class="form-control" id="indirizzoImmagine" required />
+
+        <label for="prezzo">prezzo</label>
+        <input type="number" placeholder="prezzo" class="form-control mb-2" id="prezzo" required />
+
+        <button type="submit" onclick="creaOggetto(event)" id="creaOModifica" class="btn btn-info me-2">
+          crea oggetto
+        </button>
+        <button onclick="eliminaOggetto(event)" id="delete" class="d-none btn btn-danger">cancel?</button>
       </div>
-    </div>`;
+    </form>`;
+      let eliminate = document.getElementById("delete");
+      let modifiOcrea = document.getElementById("creaOModifica");
+
+      let campiInput = document.getElementsByTagName("input");
+      let form = document.querySelector("form");
     }
   });
 
@@ -177,6 +205,34 @@ window.onload = async () => {
         }
       })
       .then((product) => {
+        card.innerHTML = `<form>
+        <div class="container">
+          <label for="nome">titolo</label>
+          <input type="text" placeholder="nome" class="form-control" id="nome" required />
+  
+          <label for="genere">genere</label>
+          <input type="text" placeholder="genere" class="form-control" id="genere" required />
+  
+          <label for="brand">brand</label>
+          <input type="text" placeholder="brand" class="form-control" id="brand" required />
+  
+          <label for="indirizzo immagine">indirizzo immagine</label>
+          <input type="text" placeholder="indirizzo immagine" class="form-control" id="indirizzoImmagine" required />
+  
+          <label for="prezzo">prezzo</label>
+          <input type="number" placeholder="prezzo" class="form-control mb-2" id="prezzo" required />
+  
+          <button type="submit" onclick="creaOggetto(event)" id="creaOModifica" class="btn btn-info me-2">
+            crea oggetto
+          </button>
+          <button onclick="eliminaOggetto(event)" id="delete" class="d-none btn btn-danger">cancel?</button>
+        </div>
+      </form>`;
+        let eliminate = document.getElementById("delete");
+        let modifiOcrea = document.getElementById("creaOModifica");
+
+        let campiInput = document.getElementsByTagName("input");
+        let form = document.querySelector("form");
         eliminate.classList.remove("d-none");
         console.log(product);
         modifiOcrea.innerText = "modifica l'oggetto";
@@ -194,6 +250,7 @@ window.onload = async () => {
 
 const creaOggetto = async (e) => {
   e.preventDefault();
+  let campiInput = document.getElementsByTagName("input");
   if (
     campiInput[0].value !== "" &&
     campiInput[1].value !== "" &&
@@ -233,6 +290,7 @@ const creaOggetto = async (e) => {
 };
 const eliminaOggetto = async (e) => {
   e.preventDefault();
+
   const accepted = confirm("sei sicuro di volerlo cancellare? guarda che non torni più indietro");
   if (accepted) {
     const resp = await fetch(url, {
--- assets/requests.js
+++ assets/requests.js
@@ -1,7 +1,9 @@
+//--------------------------variabili globali--------------
 let postoProdotti = document.querySelector("#postoProdotti");
 let depositoUrl = "https://striveschool-api.herokuapp.com/api/product";
-let spin = document.querySelector(".spinner-grow");
+let spinner = document.getElementById("allSpinner");
 let main = document.querySelector(".sotto");
+//--------------------------------------------------
 const prendiDati = async () => {
   try {
     fetch(depositoUrl, {
@@ -14,29 +16,29 @@ const prendiDati = async () => {
         if (requestProduct.status === 401) {
           console.log("la richiesta non è stata trovata");
           main.innerHTML = `<div class="card text-center">
-            <div class="card-header">
-              STOP!
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">ERROR 401 </h5>
-              <p class="card-text">ehi hai eseguito una richiesta non valida preparati a finire in gattabuia</p>
-              <a href="https://www.google.com/webhp?hl=it&sa=X&ved=0ahUKEwjzjMPbvvqAAxW5_7sIHWudAREQPAgI" class="btn btn-primary">no! non farlo! torno indietro</a>
-            </div>
-            <div class="card-footer text-body-secondary">
-              401
-            </div>
+          <div class="card-header">
+          STOP!
+          </div>
+          <div class="card-body">
+          <h5 class="card-title">ERROR 401 </h5>
+          <p class="card-text">ehi hai eseguito una richiesta non valida preparati a finire in gattabuia</p>
+          <a href="https://www.google.com/webhp?hl=it&sa=X&ved=0ahUKEwjzjMPbvvqAAxW5_7sIHWudAREQPAgI" class="btn btn-primary">no! non farlo! torno indietro</a>
+          </div>
+          <div class="card-footer text-body-secondary">
+          401
+          </div>
           </div>`;
         }
         if (requestProduct.status === 404) {
           console.log("la richiesta non è stata trovata");
           main.innerHTML = `<div class="card text-center">
-            <div class="card-header">
-              fatal error
-            </div>
-            <div class="card-body">
-              <h5 class="card-title">ERROR 404 </h5>
-              <p class="card-text">la pagina che cercavi non è stata trovata a meno che non cercavi proprio questa pagina in quel caso bravo l'hai trovata</p>
-              <a href="https://www.google.com/webhp?hl=it&sa=X&ved=0ahUKEwjzjMPbvvqAAxW5_7sIHWudAREQPAgI" class="btn btn-primary">torna indietro</a>
+          <div class="card-header">
+          fatal error
+          </div>
+          <div class="card-body">
+          <h5 class="card-title">ERROR 404 </h5>
+          <p class="card-text">la pagina che cercavi non è stata trovata a meno che non cercavi proprio questa pagina in quel caso bravo l'hai trovata</p>
+          <a href="https://www.google.com/webhp?hl=it&sa=X&ved=0ahUKEwjzjMPbvvqAAxW5_7sIHWudAREQPAgI" class="btn btn-primary">torna indietro</a>
             </div>
             <div class="card-footer text-body-secondary">
               404
@@ -106,21 +108,21 @@ const prendiDati = async () => {
           cartaShop.classList.add("p-0");
           cartaShop.classList.add("mt-2");
           cartaShop.innerHTML = `
-                <img src="${element.imageUrl}" class="card-img-top" alt="${element.name} "height = 124px">
-                <div class="card-body p-1 d-flex flex-column">
-                <div>
-                <h5 class="card-title">${element.name}</h5>
-                <p class="card-text">${element.description}</p>
-                <p class="card-text">price:${element.price}$</p>
-                </div>
-                <div class = "d-flex mt-auto">
-                <a class="btn btn-info me-auto p-2" href = "details.html?ProdId=${element._id}">Scopri di più</a>
-                <a class="btn btn-primary p-2" href = "backoffice.html?ProdId=${element._id}">modifica</a>
-                </div>
-                </div>
-                </div>`;
+          <img src="${element.imageUrl}" class="card-img-top" alt="${element.name} "height = 124px">
+          <div class="card-body p-1 d-flex flex-column">
+          <div>
+          <h5 class="card-title">${element.name}</h5>
+          <p class="card-text">${element.description}</p>
+          <p class="card-text">price:${element.price}$</p>
+          </div>
+          <div class = "d-flex mt-auto">
+          <a class="btn btn-info me-auto p-2" href = "details.html?ProdId=${element._id}">Scopri di più</a>
+          <a class="btn btn-primary p-2" href = "backoffice.html?ProdId=${element._id}">modifica</a>
+          </div>
+          </div>
+          </div>`;
+          spinner.classList.add("d-none");
           postoProdotti.appendChild(cartaShop);
-          spin.classList.add("d-none");
         });
       })
       .catch((err) => console.log(err));
