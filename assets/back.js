868e0119643c949fccb60a873175eaec6422d810
alessandro baiocco <baioccoalessandro97@gmail.com>
2023-08-26 16:05:42 +0200

    error contain

--- assets/back.js
+++ assets/back.js
@@ -3,9 +3,101 @@ const url = productId
   ? `https://striveschool-api.herokuapp.com/api/product/${productId}`
   : "https://striveschool-api.herokuapp.com/api/product";
 let campiInput = document.getElementsByTagName("input");
+let form = document.querySelector("form");
+let card = document.querySelector(".card");
 window.onload = async () => {
   let eliminate = document.getElementById("delete");
   let modifiOcrea = document.getElementById("creaOModifica");
+  fetch(url, {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NTM2YmMwMzRmZjAwMTQwM2Y0ZDEiLCJpYXQiOjE2OTI5NDczMDcsImV4cCI6MTY5NDE1NjkwN30.5tO4RfH6s6pW0wftZlqBwFHrumuQeasOT0c0yRSRoco",
+      "Content-Type": "application/json",
+    },
+  }).then((strprod) => {
+    if (strprod.status === 401) {
+      console.log("la richiesta non è stata trovata");
+      card.innerHTML = `<div class="card text-center">
+      <div class="card-header">
+        STOP!
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">ERROR 401 </h5>
+        <p class="card-text">ehi hai eseguito una richiesta non valida preparati a finire in gattabuia</p>
+        <a href="index.html" class="btn btn-primary">no! non farlo! torno indietro</a>
+      </div>
+      <div class="card-footer text-body-secondary">
+        401
+      </div>
+    </div>`;
+    }
+    if (strprod.status === 404) {
+      console.log("la richiesta non è stata trovata");
+      card.innerHTML = `<div class="card text-center">
+      <div class="card-header">
+        fatal error
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">ERROR 404 </h5>
+        <p class="card-text">la pagina che cercavi non è stata trovata a meno che non cercavi proprio questa pagina in quel caso bravo l'hai trovata</p>
+        <a href="index.html" class="btn btn-primary">torna indietro</a>
+      </div>
+      <div class="card-footer text-body-secondary">
+        404
+      </div>
+    </div>`;
+    }
+    if (strprod.status === 500) {
+      console.log("generic server error");
+      card.innerHTML = `<div class="card text-center">
+      <div class="card-header">
+        fatal error
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">ERROR 500 </h5>
+        <p class="card-text">un'errore generico del server hmmm molto strano</p>
+        <a href="index.html" class="btn btn-primary">torna indietro</a>
+      </div>
+      <div class="card-footer text-body-secondary">
+        500
+      </div>
+    </div>`;
+    }
+    if (strprod.status === 503) {
+      console.log("server gets stop");
+      card.innerHTML = `<div class="card text-center">
+      <div class="card-header">
+        fatal error
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">ERROR 503 </h5>
+        <p class="card-text">il criceto che alimentava il server è andato in pausa caffè stiamo lavorando per farlo ripartire</p>
+        <a href="index.html" class="btn btn-primary">torna indietro</a>
+      </div>
+      <div class="card-footer text-body-secondary">
+        503
+      </div>
+    </div>`;
+    }
+    if (strprod.status === 418) {
+      console.log("server gets stop");
+      card.innerHTML = `<div class="card text-center">
+      <div class="card-header">
+        fatal error
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">ERROR 418</h5>
+        <p class="card-text">il server si rifiuta di farti un caffè perchè è un server inospitale (oltre che è una teiera da the)</p>
+        <a href="index.html" class="btn btn-primary">fammi un thè invece</a>
+      </div>
+      <div class="card-footer text-body-secondary">
+        418
+      </div>
+    </div>`;
+    }
+  });
+
+  //-------------------------------------------------------------------------
   if (productId) {
     fetch(url, {
       headers: {
@@ -15,8 +107,74 @@ window.onload = async () => {
       },
     })
       .then((strprod) => {
-        console.log(strprod);
-        return strprod.json();
+        if (strprod.status === 404) {
+          console.log("la richiesta non è stata trovata");
+          card.innerHTML = `<div class="card text-center">
+          <div class="card-header">
+            fatal error
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">ERROR 404 </h5>
+            <p class="card-text">la pagina che cercavi non è stata trovata a meno che non cercavi proprio questa pagina in quel caso bravo l'hai trovata</p>
+            <a href="index.html" class="btn btn-primary">torna indietro</a>
+          </div>
+          <div class="card-footer text-body-secondary">
+            404
+          </div>
+        </div>`;
+        }
+        if (strprod.status === 500) {
+          console.log("generic server error");
+          card.innerHTML = `<div class="card text-center">
+          <div class="card-header">
+            fatal error
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">ERROR 500 </h5>
+            <p class="card-text">un'errore generico del server hmmm molto strano</p>
+            <a href="index.html" class="btn btn-primary">torna indietro</a>
+          </div>
+          <div class="card-footer text-body-secondary">
+            500
+          </div>
+        </div>`;
+        }
+        if (strprod.status === 503) {
+          console.log("server gets stop");
+          card.innerHTML = `<div class="card text-center">
+          <div class="card-header">
+            fatal error
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">ERROR 503 </h5>
+            <p class="card-text">il criceto che alimentava il server è andato in pausa caffè stiamo lavorando per farlo ripartire</p>
+            <a href="index.html" class="btn btn-primary">torna indietro</a>
+          </div>
+          <div class="card-footer text-body-secondary">
+            503
+          </div>
+        </div>`;
+        }
+        if (strprod.status === 418) {
+          console.log("server gets stop");
+          card.innerHTML = `<div class="card text-center">
+          <div class="card-header">
+            fatal error
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">ERROR 418</h5>
+            <p class="card-text">il server si rifiuta di farti un caffè perchè è un server inospitale (oltre che è una teiera da the)</p>
+            <a href="index.html" class="btn btn-primary">fammi un thè invece</a>
+          </div>
+          <div class="card-footer text-body-secondary">
+            418
+          </div>
+        </div>`;
+        }
+
+        if (strprod.ok) {
+          return strprod.json();
+        }
       })
       .then((product) => {
         eliminate.classList.remove("d-none");
@@ -27,6 +185,9 @@ window.onload = async () => {
           (campiInput[2].value = product.brand),
           (campiInput[3].value = product.imageUrl),
           (campiInput[4].value = product.price);
+      })
+      .catch((err) => {
+        console.log(err);
       });
   }
 };
@@ -80,7 +241,16 @@ const eliminaOggetto = async (e) => {
         Authorization:
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NTM2YmMwMzRmZjAwMTQwM2Y0ZDEiLCJpYXQiOjE2OTI5NDczMDcsImV4cCI6MTY5NDE1NjkwN30.5tO4RfH6s6pW0wftZlqBwFHrumuQeasOT0c0yRSRoco",
       },
-    }).then((resp) => window.location.assign("index.html"));
+    })
+      .then((resp) => window.location.assign("index.html"))
+      .catch((err) => {
+        if (err.status === 404) {
+          console.log("la richiesta non è stata trovata");
+        }
+        if (err.status === 500) {
+          console.log("generic server error");
+        }
+      });
   } else {
     alert("operazione annullata");
   }
--- assets/requests.js
+++ assets/requests.js
@@ -1,6 +1,7 @@
 let postoProdotti = document.querySelector("#postoProdotti");
 let depositoUrl = "https://striveschool-api.herokuapp.com/api/product";
 let spin = document.querySelector(".spinner-grow");
+let main = document.querySelector(".sotto");
 const prendiDati = async () => {
   try {
     fetch(depositoUrl, {
@@ -9,7 +10,92 @@ const prendiDati = async () => {
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NTM2YmMwMzRmZjAwMTQwM2Y0ZDEiLCJpYXQiOjE2OTI5NDczMDcsImV4cCI6MTY5NDE1NjkwN30.5tO4RfH6s6pW0wftZlqBwFHrumuQeasOT0c0yRSRoco",
       },
     })
-      .then((requestProduct) => requestProduct.json())
+      .then((requestProduct) => {
+        if (requestProduct.status === 401) {
+          console.log("la richiesta non è stata trovata");
+          main.innerHTML = `<div class="card text-center">
+            <div class="card-header">
+              STOP!
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ERROR 401 </h5>
+              <p class="card-text">ehi hai eseguito una richiesta non valida preparati a finire in gattabuia</p>
+              <a href="https://www.google.com/webhp?hl=it&sa=X&ved=0ahUKEwjzjMPbvvqAAxW5_7sIHWudAREQPAgI" class="btn btn-primary">no! non farlo! torno indietro</a>
+            </div>
+            <div class="card-footer text-body-secondary">
+              401
+            </div>
+          </div>`;
+        }
+        if (requestProduct.status === 404) {
+          console.log("la richiesta non è stata trovata");
+          main.innerHTML = `<div class="card text-center">
+            <div class="card-header">
+              fatal error
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ERROR 404 </h5>
+              <p class="card-text">la pagina che cercavi non è stata trovata a meno che non cercavi proprio questa pagina in quel caso bravo l'hai trovata</p>
+              <a href="https://www.google.com/webhp?hl=it&sa=X&ved=0ahUKEwjzjMPbvvqAAxW5_7sIHWudAREQPAgI" class="btn btn-primary">torna indietro</a>
+            </div>
+            <div class="card-footer text-body-secondary">
+              404
+            </div>
+          </div>`;
+        }
+        if (requestProduct.status === 500) {
+          console.log("generic server error");
+          main.innerHTML = `<div class="card text-center">
+            <div class="card-header">
+              fatal error
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ERROR 500 </h5>
+              <p class="card-text">un'errore generico del server hmmm molto strano</p>
+              <a href="https://www.google.com/webhp?hl=it&sa=X&ved=0ahUKEwjzjMPbvvqAAxW5_7sIHWudAREQPAgI" class="btn btn-primary">torna indietro</a>
+            </div>
+            <div class="card-footer text-body-secondary">
+              500
+            </div>
+          </div>`;
+        }
+        if (requestProduct.status === 503) {
+          console.log("server gets stop");
+          main.innerHTML = `<div class="card text-center">
+            <div class="card-header">
+              fatal error
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ERROR 503 </h5>
+              <p class="card-text">il criceto che alimentava il server è andato in pausa caffè stiamo lavorando per farlo ripartire</p>
+              <a href="https://www.google.com/webhp?hl=it&sa=X&ved=0ahUKEwjzjMPbvvqAAxW5_7sIHWudAREQPAgI" class="btn btn-primary">torna indietro</a>
+            </div>
+            <div class="card-footer text-body-secondary">
+              503
+            </div>
+          </div>`;
+        }
+        if (requestProduct.status === 418) {
+          console.log("server gets stop");
+          main.innerHTML = `<div class="card text-center">
+            <div class="card-header">
+              fatal error
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">ERROR 418</h5>
+              <p class="card-text">il server si rifiuta di farti un caffè perchè è un server inospitale (oltre che è una teiera da the)</p>
+              <a href="https://www.google.com/webhp?hl=it&sa=X&ved=0ahUKEwjzjMPbvvqAAxW5_7sIHWudAREQPAgI" class="btn btn-primary">fammi un thè invece</a>
+            </div>
+            <div class="card-footer text-body-secondary">
+              418
+            </div>
+          </div>`;
+        }
+
+        if (requestProduct.ok) {
+          return requestProduct.json();
+        }
+      })
       .then((product) => {
         product.forEach((element) => {
           let cartaShop = document.createElement("div");
